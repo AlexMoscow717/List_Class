2838abc2bb2c4197f23baf792fdae835f73c64db
Алексей Москвичев <aleks_moscow@outlook.com>
2021-03-08 23:01:12 +0300

    test 2

--- List_Class/List_Class.cpp
+++ List_Class/List_Class.cpp
@@ -7,6 +7,8 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "test git 1 \n";
+    std::cout << "test git 2 \n";
+
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
